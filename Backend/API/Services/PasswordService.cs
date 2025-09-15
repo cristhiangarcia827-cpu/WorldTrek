dbc6420494daf51ca889fbf06dd07ab8c76a3339
Cristhian Garcoa <cristhiangarcia827@gmail.com>
2025-09-14 19:24:52 -0600

    Conexion con firebase y autenticacion y registro de usuario

---
+++ Backend/API/Services/PasswordService.cs
@@ -0,0 +1,17 @@
+﻿using BC = BCrypt.Net.BCrypt;
+
+namespace API.Services
+{
+    public class PasswordService
+    {
+        public string HashPassword(string password)
+        {
+            return BC.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string hash)
+        {
+            return BC.Verify(password, hash);
+        }
+    }
+}
